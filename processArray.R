a6b38deb18a8fc7af1b39879d499fab685c395b3
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-10 14:30:56 -0500

    Create processArray.R

---
+++ processArray.R
@@ -0,0 +1,14 @@
+library(ToxicoGx)
+library(gdata)
+library(affy)
+library(Biobase)
+library(xml2)
+library(abind)
+library("rat2302rnensgcdf")
+
+cdf <- "rat2302rnensgcdf"
+
+xx <- list.files("/pfs/TGRatArray/TGGATES_rat_CELfiles", full.names = T, "\\.CEL$")
+eset <- just.rma(filenames = xx, verbose = TRUE, cdfname = cdf)
+
+saveRDS(eset, "/pfs/out/eset_Rat_3276.rds")
