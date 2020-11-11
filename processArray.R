f768f986d43f90b49485ec05eea54c1d87ed01c8
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-11 11:32:47 -0500

    Update processArray.R

--- processArray.R
+++ processArray.R
@@ -8,7 +8,10 @@ library("rat2302rnensgcdf")
 
 cdf <- "rat2302rnensgcdf"
 
+rat_cels <- read.csv("/pfs/processTGGATEarray_rat/rat_cel.csv", sep = "\t")
+celfn <- paste0("/pfs/TGRatArray/TGGATES_rat_CELfiles/", rat_cels$x)
 #xx <- list.files("/pfs/TGRatArray/TGGATES_rat_CELfiles", full.names = T, "\\.CEL$")
-#eset <- just.rma(filenames = xx, verbose = TRUE, cdfname = cdf)
 
-#saveRDS(eset, "/pfs/out/eset_Rat_3276.rds")
+eset <- just.rma(filenames = celfn, verbose = TRUE, cdfname = cdf)
+
+saveRDS(eset, "/pfs/out/eset_Rat_3276.rds")
